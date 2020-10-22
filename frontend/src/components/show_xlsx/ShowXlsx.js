e1e6c597b691563e523b5d8281b0ea9155918815
XandRules <alexandresilva58@gmail.com>
2020-10-22 18:12:47 -0300

    criado componente para listar os dados

--- frontend/src/components/show_xlsx/ShowXlsx.js
+++ frontend/src/components/show_xlsx/ShowXlsx.js
@@ -1,44 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import axios from 'axios';
 
-
-export default class ShowXlsx extends Component{
-  
-  state = { 
-    fileName : null,
-  }; 
-  
-  getList(){
-    return axios.get(`http://localhost:3333/list/${this.props.fileName}`).then(response =>{
-      console.log(response.data.result);
-    });
-  }
-  
-  render() {
-    return (
-      <div className="uk-container" onChange={this.props.onChange}>
-          <table className="uk-table uk-table-divider">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Sobrenome</th>
-                    <th>CPF</th>
-                </tr>
-            </thead>
-            <tbody>
-              {/* {resp.map(i =>{ */}
-               <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-              {/* })} */}
-            </tbody>
-        </table>
-      </div>
-    )    
+function getList(fileName){
+  return axios.get(`http://localhost:3333/list/${fileName}`);
 }
 
+const ShowXlsx = ({fileName}) =>{
+  
+  let resp = {};
+
+  getList(fileName).then(response =>{
+    console.log(response.data.result)
+
+    resp = response.data.result;
+
+    resp.map(i =>{
+      console.log(i[0], i[1], i[2]);
+    })
+
+  });
+
+  return (
+    <div className="uk-container">
+        <table className="uk-table uk-table-divider">
+          <thead>
+              <tr>
+                  <th>Nome</th>
+                  <th>Sobrenome</th>
+                  <th>CPF</th>
+              </tr>
+          </thead>
+          <tbody>
+            {/* {resp.map(i =>{ */}
+             <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+            {/* })} */}
+          </tbody>
+      </table>
+    </div>
+  )
 
 }
+
+export default ShowXlsx;
